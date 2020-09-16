bd86a8df01b36a1a6235fbb68621012e560254ed
Ethan <eleetyson@gmail.com>
2020-09-15 21:00:24 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :patient
+
+  def doctor_name
+    self.doctor ? self.doctor.name : nil
+  end
+
+  def patient_name
+    self.patient ? self.patient.name : nil
+  end
+end
