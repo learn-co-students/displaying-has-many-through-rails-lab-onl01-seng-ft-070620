968b653bfc0edd078279f0fb78d78fdccf86ef57
Amanda_Katherine <walker.akw@gmail.com>
2020-09-18 00:25:19 -0400

    Done.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,4 @@
 class Doctor < ApplicationRecord
     has_many :appointments
     has_many :patients, through: :appointments
-
-    def appointment_time(time)
-        self.appointment_datetime = Appointment.find_or_create_by(time: time)
-    end
 end
